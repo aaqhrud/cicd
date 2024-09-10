8e2d53da9a5d8329fc11fddd8bc03313addecda3
bkhan <aaqhrud@naver.com>
2024-09-10 10:25:41 +0900

    #1 feat5: Init my app

--- my-app/tests/test_main.py
+++ my-app/tests/test_main.py
@@ -2,5 +2,5 @@ from my_app.mian import add
 
 def test_add():
     assert add(2,3) == 5
-    assert add(-1,3) == 5
-    assert add(2,5) == 1000
+   # assert add(-1,3) == 5
+   # assert add(2,5) == 1000
